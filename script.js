536e65c2b97ee7d8cd3121a9d508e0a21e1ff2ea
Felipe Holanda <93724958+Felipe-Holanda@users.noreply.github.com>
2022-05-31 16:34:22 -0300

    Adição dos arquivos
    Adição dos arquivos iniciais.

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('especial').style.display = 'none';
+
+function fecha_nav() {
+    document.getElementById('newsletter-form').style.display = 'none';
+    document.getElementById('nav_fecha').style.display = 'none';
+    document.getElementById('especial').style.display = '';
+}
+
+function aviso() {
+    alert('Olá! Esta página é uma sátira!\n' +
+        'nenhum botão ou link é definitivamente funcional\n');
+}
+
+function abrenav() {
+    document.getElementById('newsletter-form').style.display = '';
+    document.getElementById('nav_fecha').style.display = '';
+    document.getElementById('especial').style.display = 'none';
+}
